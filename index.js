fb8dabfdc321b98578ea2cbf955df18dad6ecc47
Zero <kahonide@gmail.com>
2022-04-29 15:12:32 +0200

    change

--- index.js
+++ index.js
@@ -31,7 +31,31 @@ let PORT = process.env.PORT || 8080;
 
 app.get("/cors", (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
-  res.send({ msg: "This has CORS enabled 🎈" });
+  const { body } = req;
+  if (body.token == process.env.EMAILTOKEN) {
+    const options = {
+      from: process.env.EMAIL,
+      to: body.toemail,
+      subject: body.subject,
+      template: "email",
+      context: body.context,
+    };
+
+    transporter.sendMail(options, (err, info) => {
+      let state = "";
+      if (err) {
+        console.log(err);
+        state = err;
+      } else {
+        console.log(info.response);
+        state = "email sent";
+      }
+
+      res.send(state);
+    });
+  } else {
+    res.send("token no identified");
+  }
 });
 
 app.use(express.json());
